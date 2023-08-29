1cb5162b14d37d3c6e33784354667b47aeb61ade
Jake Nead <jakenead@gmail.com>
2023-08-29 16:59:05 -0700

    Country input working

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const form = document.querySelector('form');
 const email = document.querySelector('#email');
 const emailError = document.querySelector('#emailError');
 const country = document.querySelector('#country');
-const countryError = document.querySelector('#country');
+const countryError = document.querySelector('#countryError');
 const zip = document.querySelector('#zip');
 const zipError = document.querySelector('#zipError');
 const password = document.querySelector('#password');
 const passwordError = document.querySelector('#passwordError');
 
-email.addEventListener('input', (e) => {
+email.addEventListener('input', () => {
   if (email.validity.valid) {
     emailError.textContent = '';
     emailError.className = 'error';
@@ -21,64 +21,66 @@ function showEmailError() {
   if (email.validity.valueMissing) {
     emailError.textContent = 'Please  enter an email address';
   } else if (email.validity.typeMismatch) {
-    emailError.textContent = 'Please enter a real email address';
+    emailError.textContent = 'Please enter a valid email address';
   } else if (email.validity.tooShort) {
     emailError.textContent = `Email needs to be at least ${email.minLength} characters.`;
   }
   emailError.className = 'error active';
 }
 
-country.addEventListener('input', (e) => {
-  if (!country.value === '') {
+country.addEventListener('change', () => {
+  console.log(country.value === '');
+  if (!country.value == '') {
     countryError.textContent = '';
     countryError.className = 'error';
   } else {
     showCountryError();
   }
-  console.log(country);
+//   console.log(country.value);
 });
 
 function showCountryError() {
   countryError.textContent = 'Please select a country';
+  countryError.className = 'error active';
 }
 
-zip.addEventListener('input', () => {
-  if (zip.validity.value) {
-    zipError.textContent = '';
-    zipError.className = 'error';
-  } else {
-    showZipError();
-  }
-});
+// zip.addEventListener('input', () => {
+//   if (zip.validity.valid) {
+//     zipError.textContent = '';
+//     zipError.className = 'error';
+//   } else {
+//     showZipError();
+//   }
+// });
 
-function showZipError() {
-  if (zip.validity.valueMissing) {
-    zipError.textContent = 'Please  enter a zip code';
-  } else if (zip.validity.tooShort) {
-    zipError.textContent = `Zip code needs to be at least ${zip.minLength} numbers.`;
-  }
-  zipError.className = 'error active';
-}
+// function showZipError() {
+//   if (zip.validity.valueMissing) {
+//     zipError.textContent = 'Please  enter a zip code';
+//   } else if (zip.validity.tooShort) {
+//     zipError.textContent = `Zip code needs to be at least ${zip.minLength} numbers.`;
+//   }
+//   zipError.className = 'error active';
+// }
 // function showPasswordError() {
 // }
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log(country.value);
   if (!email.validity.valid) {
     showEmailError();
     e.preventDefault();
   }
   if (country.value === '') {
+    console.log('test');
     showCountryError();
     e.preventDefault();
   }
-  if (!zip.validity.valid) {
-    showZipError();
-    e.preventDefault();
-  }
-  if (!password.validity.valid) {
-    showPasswordError();
-    e.preventDefault();
-  }
+//   if (!zip.validity.valid) {
+//     showZipError();
+//     e.preventDefault();
+//   }
+//   if (!password.validity.valid) {
+//     // showPasswordError();
+//     e.preventDefault();
+//   }
 });
